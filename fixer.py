8c924e8ad4b89bb3f380369a1c731561660ee2c4
Xander Goebel <xandergoebel1@gmail.com>
2024-09-03 19:28:15 -0400

    system-agnostic pathing

--- fixer.py
+++ fixer.py
@@ -25,22 +25,14 @@ def clocktower_json_fixer(new_char_file, replacements, filename):
         text = jsons[x]
         script = script.replace(character, text)
 
-    if sys.platform.startswith('darwin'):
-        # exporting the file
-        part = filename.split(".")[1].split("/")[2]
-        new_filename = f"./out/{part}_fixed.json"
-        with open(new_filename, 'w', encoding='utf-8') as fh:
-            fh.write(script)
-        print("Writing to",new_filename)
-    elif sys.platform.startswith('win32'):
-        # exporting the file
-        part = filename.split(".")[1].split("\\")[2]
-        new_filename = f".\\out\\{part}_fixed.json"
-        with open(new_filename, 'w', encoding='utf-8') as fh:
-            fh.write(script)
-        print("Writing to",new_filename)
+    # exporting the file
+    tail = os.path.split(filename)[-1].split(".")[0]
+    new_filename = f"{tail}_fixed.json"
+    output_path = os.path.join(".","out", new_filename)
+    with open(output_path, 'w', encoding='utf-8') as fh:
+        fh.write(script)
+    print("Writing to",new_filename)
 
-if sys.platform.startswith('darwin'):
-    clocktower_json_fixer('./roles.txt','./roles.json',sys.argv[1])
-elif sys.platform.startswith('win32'):
-    clocktower_json_fixer('.\\roles.txt','.\\roles.json',sys.argv[1])
+txt_file = os.path.join(".","roles.txt")
+json_file = os.path.join(".","roles.json")
+clocktower_json_fixer(txt_file, json_file, sys.argv[1])
